712fd453e6984c7d2a8679f21c4d7c78dc758bcf
unknown <ccao2776@gmail.com>
2023-05-24 15:27:51 -0700

    TODO: images

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import pygame
+from pygame.time import Clock
 
-WIDTH, HEIGHT = 450, 250
+# define window elements
+WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Ttoki Hunt!")
+WHITE = (255, 255, 255)
+FPS = 60
+
+def draw_window():
+    WIN.fill((WHITE))
+    pygame.display.update()
 
 def main():
-    
+
     # enable event loop
+    clock = Clock()
     run = True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-    
+            draw_window()
+
     pygame.quit()
 
 if __name__ == '__main__':
